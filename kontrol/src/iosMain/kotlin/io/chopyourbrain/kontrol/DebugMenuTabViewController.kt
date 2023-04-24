54b2b99caa3d0d98ad89fda70c9c2cbace36b861
Viktor Savchik <viktor.savchik@idfinance.com>
2023-04-24 15:38:30 +0200

    *changes according to ktlint

--- kontrol/src/iosMain/kotlin/io/chopyourbrain/kontrol/DebugMenuTabViewController.kt
+++ kontrol/src/iosMain/kotlin/io/chopyourbrain/kontrol/DebugMenuTabViewController.kt
@@ -5,27 +5,41 @@ import io.chopyourbrain.kontrol.properties.PropertiesViewController
 import io.chopyourbrain.kontrol.properties.getPropertiesAsString
 import kotlinx.cinterop.ExportObjCClass
 import kotlinx.cinterop.ObjCAction
-import kotlinx.cinterop.ObjCObjectBase
 import platform.Foundation.NSBundle
 import platform.Foundation.NSSelectorFromString
-import platform.UIKit.*
+import platform.UIKit.UIActivityViewController
+import platform.UIKit.UIBarButtonItem
+import platform.UIKit.UIBarButtonItemStyle
+import platform.UIKit.UIImage
+import platform.UIKit.UITabBarController
+import platform.UIKit.UITabBarControllerDelegateProtocol
+import platform.UIKit.UITabBarItem
+import platform.UIKit.UIViewController
+import platform.UIKit.navigationItem
+import platform.UIKit.popoverPresentationController
+import platform.UIKit.tabBarItem
 
 @ExportObjCClass
 class DebugMenuTabViewController @OverrideInit constructor(
     nibName: String? = null,
-    bundle: NSBundle? = null
+    bundle: NSBundle? = null,
 ) : UITabBarController(nibName, bundle), UITabBarControllerDelegateProtocol {
 
     override fun viewDidLoad() {
         super.viewDidLoad()
         val controllerList = mutableListOf<UIViewController>()
         val debugViewController = PropertiesViewController()
-        debugViewController.tabBarItem = UITabBarItem("Properties", UIImage.systemImageNamed("slider.horizontal.3"), 0L)
+        debugViewController.tabBarItem =
+            UITabBarItem("Properties", UIImage.systemImageNamed("slider.horizontal.3"), 0L)
         controllerList.add(debugViewController)
         if (ServiceLocator.DBRepository.value != null) {
             val networkViewController = NetworkViewController()
             networkViewController.tabBarItem =
-                UITabBarItem("Network", UIImage.systemImageNamed("antenna.radiowaves.left.and.right"), 1L)
+                UITabBarItem(
+                    "Network",
+                    UIImage.systemImageNamed("antenna.radiowaves.left.and.right"),
+                    1L,
+                )
             controllerList.add(networkViewController)
         }
         this.viewControllers = controllerList.toList()
@@ -35,8 +49,9 @@ class DebugMenuTabViewController @OverrideInit constructor(
                 UIImage.systemImageNamed("square.and.arrow.up"),
                 UIBarButtonItemStyle.UIBarButtonItemStylePlain,
                 this,
-                NSSelectorFromString(::onShareTapped.name)
-            ), true
+                NSSelectorFromString(::onShareTapped.name),
+            ),
+            true,
         )
     }
 
@@ -48,8 +63,11 @@ class DebugMenuTabViewController @OverrideInit constructor(
         this.presentViewController(activityViewController, true, null)
     }
 
-    @Suppress("CONFLICTING_OVERLOADS","RETURN_TYPE_MISMATCH_ON_OVERRIDE")
-    override fun tabBarController(tabBarController: UITabBarController, shouldSelectViewController: UIViewController): Boolean {
+    @Suppress("CONFLICTING_OVERLOADS", "RETURN_TYPE_MISMATCH_ON_OVERRIDE")
+    override fun tabBarController(
+        tabBarController: UITabBarController,
+        shouldSelectViewController: UIViewController,
+    ): Boolean {
         return true
     }
 }
